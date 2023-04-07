3d5ece1181fedcf8ff791e8cb17ec05cd0a5fa90
kurniariyan110304 <kurniariyan110304@gmail.com>
2023-04-07 23:30:51 +0700

    upload pdo-main

---
+++ praktikum05/databasephp-main/databasephp-main/form_produk.php
@@ -0,0 +1,144 @@
+<?php 
+require_once 'dbkoneksi.php';
+?>
+            
+<form method="POST" action="proses_produk.php">
+  <div class="form-group row">
+    <label for="kode" class="col-4 col-form-label">Kode</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-anchor"></i>
+          </div>
+        </div> 
+        <input id="kode" name="kode" type="text" class="form-control"
+        value="">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nama" class="col-4 col-form-label">Nama Produk</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-adjust"></i>
+          </div>
+        </div> 
+        <input id="nama" name="nama" type="text" class="form-control" 
+        value="">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="harga_beli" class="col-4 col-form-label">Harga Beli</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-arrow-circle-o-left"></i>
+          </div>
+        </div> 
+        <input id="harga_beli" name="harga_beli" 
+        value="" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="stok" class="col-4 col-form-label">Stok</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-arrow-circle-up"></i>
+          </div>
+        </div> 
+        <input id="stok" name="stok" value=""
+        type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="min_stok" class="col-4 col-form-label">Minimum Stok</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-arrow-circle-right"></i>
+          </div>
+        </div> 
+        <input id="min_stok" name="min_stok" 
+        value=""
+        type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="jenis" class="col-4 col-form-label">Jenis Produk</label> 
+    <div class="col-8">
+        <?php 
+            $sqljenis = "SELECT * FROM jenis_produk";
+            $rsjenis = $dbh->query($sqljenis);
+        ?>
+      <select id="jenis" name="jenis" class="custom-select">
+          <?php 
+            foreach($rsjenis as $rowjenis){
+         ?>
+            <option value="<?=$rowjenis['id']?>"><?=$rowjenis['nama']?></option>
+         <?php
+            }
+        ?>
+        <!--
+        <option value="1">Elektronik</option>
+        <option value="2">Furniture</option>
+        <option value="3">Makanan</option>-->
+
+      </select>
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <input type="submit" name="proses" type="submit" 
+      class="btn btn-primary" value="Simpan"/>
+    </div>
+  </div>
+</form>
+
+<div class="form-group row">
+    <label for="jenis" class="col-4 col-form-label">Kartu</label>
+    <div class="col-8">
+      <?php
+      $sqljenis = "SELECT * FROM kartu";
+      $rsjenis = $dbh->query($sqljenis);
+      ?>
+      <select id="kartu_id" name="kartu_id" class="custom-select">
+        <?php
+        if (isset($_GET['idedit'])) {
+          $sqlkartu = "SELECT * FROM kartu WHERE id=?";
+          $stkartu = $dbh->prepare($sqlkartu);
+          $stkartu->execute([$row['kartu_id']]);
+          $rowkartu = $stkartu->fetch();
+        ?>
+          <option value="<?= $rowkartu['id'] ?>"><?= $rowkartu['nama'] ?></option>
+        <?php
+        }
+        foreach ($rsjenis as $rowjenis) {
+        ?>
+          <option value="<?= $rowjenis['id'] ?>"><?= $rowjenis['nama'] ?></option>
+        <?php
+        }
+        ?>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <input type="submit" name="proses" type="submit" class="btn btn-primary" value="<?php if (isset($_GET['idedit'])) {
+                                                                                        echo "Update";
+                                                                                      } else {
+                                                                                        echo "Simpan";
+                                                                                      } ?>" />
+    </div>
+  </div>
+</form>
